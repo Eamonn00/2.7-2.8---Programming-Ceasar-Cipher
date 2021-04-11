0f04e7d72a5f311479a430309d444702d4ab2d1f
Eamonn00 <>
2021-04-11 22:33:57 +0000

    Added components "get input text" and "get input key"

---
+++ 01_get_input_text.py
@@ -0,0 +1,2 @@
+#ask user for input
+initial_text = input(print("Enter the message you want to encrypt"))
---
+++ 02_get_input_key.py
@@ -0,0 +1,2 @@
+#ask user for key
+index_key = int(input(print(what numbe rdo you want to use as your key?)))
