f623c4db4fc49e69aaa900bb55900f3da39098c3
Eamonn00 <>
2021-04-15 23:49:03 +0000

    Added decrypt uppercase letters function

--- 05_decrypt_upper_text.py
+++ 05_decrypt_upper_text.py
@@ -1,11 +1,10 @@
-encryption = ""
+decryption = ""
 
 for c in initial_text:
     if c.isupper():
         c_code = ord(c)
         c_pos = ord(c) - ord("A")
-        new_pos = (c_pos + index_key) % 26
+        new_pos = (c_pos - index_key) % 26
         new_c_code = new_pos + ord("A")
         new_c = chr(new_c_code)
-        encryption = encryption + new_c
-
+        decryption = decryption + new_c
--- main.py
+++ main.py
@@ -1,21 +1,41 @@
-#ask user for text to encrypt
+answer = input(print("Do you want to encrypt or decrypt a message?"))
 
-initial_text = input(print("Enter the message you want to encrypt"))
+if answer == "encrypt":
+    initial_text = input(print("Enter the message you want to encrypt"))
 
-#ask user for encryption key
 
-index_key = int(input(print("what number do you want to use as your key?")))
+    index_key = int(input(print("what number do you want to use as your key?")))
 
-#encrypt uppercase letters
+    encryption = ""
 
-encryption = ""
+    for c in initial_text:
+        if c.isupper():
+            c_code = ord(c)
+            c_pos = ord(c) - ord("A")
+            new_pos = (c_pos + index_key) % 26
+            new_c_code = new_pos + ord("A")
+            new_c = chr(new_c_code)
+            encryption = encryption + new_c
 
-for c in initial_text:
-    if c.isupper():
-        c_code = ord(c)
-        c_pos = ord(c) - ord("A")
-        new_pos = (c_pos + index_key) % 26
-        new_c_code = new_pos + ord("A")
-        new_c = chr(new_c_code)
-        encryption = encryption + new_c
+    print(encryption)
 
+
+elif answer == "decrypt":
+    
+    initial_text = input(print("Enter the message you want to decrypt"))
+
+
+    index_key = int(input(print("what number do you want to use as your key?")))
+
+
+    decryption = ""
+
+    for c in initial_text:
+        if c.isupper():
+            c_code = ord(c)
+            c_pos = ord(c) - ord("A")
+            new_pos = (c_pos - index_key) % 26
+            new_c_code = new_pos + ord("A")
+            new_c = chr(new_c_code)
+            decryption = decryption + new_c
+    print(decryption)
